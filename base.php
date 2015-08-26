75f3e10130f45ce0bef74f12de47d7566c78e199
Stuart Starr <stuart@stueynet.com>
2015-08-26 17:13:53 -0400

    post thumb

--- base.php
+++ base.php
@@ -24,6 +24,13 @@ do_action('get_header');
 get_template_part('templates/header');
 get_template_part('templates/page-hero');
 ?>
+    <?php if(is_singular('post')): ?>
+        <?php
+        $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'portfolio-featured' );
+        $url = $thumb['0'];
+        ?>
+        <div style="background-image: url(<?php echo $url; ?>)" class="post-thumbnail"></div>
+    <?php endif; ?>
 <div class="wrap container" role="document">
 	<div class="content row">
 		<main class="main" role="main">
