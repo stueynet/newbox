67d2b626566367d0b77ff2ff6f94cabc3b54199b
Stuart Starr <stuart@stueynet.com>
2015-09-24 15:43:31 -0400

    head option

--- templates/head.php
+++ templates/head.php
@@ -1,7 +1,8 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/prism/0.0.1/prism.min.css">
-  <?php wp_head(); ?>
+    <?php echo get_field('head', 'option'); ?>
+    <?php wp_head(); ?>
 </head>
